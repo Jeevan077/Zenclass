62205eac846fa64efbc53bc9c8aa666e5a836084
Jeevan077 <jeevanreddafoxx@gmail.com>
2025-01-08 18:45:09 +0530

    Task

---
+++ Callingclass.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+public class Callingclass {
+
+	public static void main(String[] args) {
+		Firstclass.subtract();
+		Firstclass objFirst=new Firstclass();
+		objFirst.get();
+		objFirst.add();
+
+	}
+
+}
